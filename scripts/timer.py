27b67bd4bce55ef4b9df9180ae0ffba915c33751
munaibalhelali <moneebalhelaly@yahoo.com>
2022-07-06 16:00:05 +0200

    added countdown, pause, continue and stop features

--- scripts/timer.py
+++ scripts/timer.py
@@ -1,43 +1,58 @@
 #!/usr/bin/env python3
 import time
+from threading import Timer as TM 
 
 class Timer:
     def __init__(self):
         self.start_time = None
-        self.loops = []
-        self.prev_loop_time = None 
+        self.duration = 0
+        self.count_down_time = 0
+        self.paused = False 
+        self.started = False
+        self.count_down = False  
 
     def start(self):
         self.start_time = time.time()
-        self.prev_loop_time = time.time()
+        self.paused = False
+        self.started = True
 
-    def record_loop(self):
-        if self.start_time is None:
-            self.start()
-        loop = time.time() - self.prev_loop_time 
-        self.loops.append(loop)
-        self.prev_loop_time = time.time()
+    def initalize_count_down(self, time_in_sec):
+        self.count_down_time = time_in_sec
+        self.start_time = time.time()
+        self.count_down = True
+
+    def pause(self):
+        self.paused = True
+
+    def continue_timer(self):
+        self.paused = False 
+        self.start_time = time.time() - self.duration
+
+    def stop(self):
+        self.reset()
+        self.paused = True
+        self.count_down = False
 
     def reset(self):
-       self.start_time = time.time()
+        self.start_time = time.time()
+        self.duration = 0
+        self.paused = False 
+        self.started = False
 
     def get_timer_duration(self):
-        if self.start_time is None:
-            self.start()
-        return time.time() - self.start_time
+        return self.duration
 
     def get_formated_timer_duration(self):
-        if self.start_time is None:
-            self.start()
-        t = time.time() - self.start_time
+        t = self.duration if not self.count_down else (self.count_down_time-self.duration)
         H = int(t//3600)
-        H = f'{H}' if H > 9 else f'0{H}'
         M = int((t%3600)//60)
-        M =  f'{M}' if M > 9 else f'0{M}'
         S = int((t%3600)%60)
-        S =  f'{S}' if S > 9 else f'0{S}'
-        str_time = f'{H}:{M}:{S}'
-        return str_time
+        time_str = "%02d:%02d:%02d" % (H, M, S)
+        return time_str
 
-
-
+    def update(self):
+        if not self.paused and self.started:
+            self.duration = time.time() - self.start_time
+            if self.count_down:
+                if self.count_down_time - self.duration <= 0:
+                    self.stop()
